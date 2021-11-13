2183fd85bc08999162d185071aff9b409ef33baa
platnate <morrisnl24@outlook.com>
2021-11-13 16:06:27 -0600

    Pathfinding step 1

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -1,14 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AI : MonoBehaviour
 {
-    
+    NavMeshAgent navMeshAgent;
+    public Transform target;
+    public Transform player;
+    public State state;
+    public bool arrivedAtTarget;
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+
     private void Update()
     {
+        navMeshAgent.destination = target.position;
+        arrivedAtTarget = navMeshAgent.remainingDistance < 1.5f;
+
+        Vector3 rotation = player.position - transform.position;
+        rotation = rotation.normalized;
+
+
         RaycastHit hit;
-        Physics.Raycast(transform.position, Vector3.forward, out hit, Mathf.Infinity);
+        Physics.Raycast(transform.position, rotation, out hit, Mathf.Infinity);
         Debug.Log(hit.transform);
     }
+    private void OnDrawGizmos()
+    {
+        if (player != null)
+        Gizmos.DrawLine(player.position, transform.position);
+    }
 }
+
+public enum State { searching, idle, chasing, fleaing, attacking };
